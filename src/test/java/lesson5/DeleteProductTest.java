4da33cbda1337887096ce3a7910de2c5be7a7092
geekbrains <kuznecova_tat88@mail.ru>
2022-03-05 10:15:23 +0300

    init

---
+++ src/test/java/lesson5/DeleteProductTest.java
@@ -0,0 +1,51 @@
+package lesson5;
+
+import com.github.javafaker.Faker;
+import lesson5.api.ProductService;
+import lesson5.dto.Product;
+import lesson5.utils.RetrofitUtils;
+import lombok.SneakyThrows;
+import okhttp3.ResponseBody;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.*;
+import retrofit2.Response;
+
+import java.io.IOException;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+
+public class DeleteProductTest {
+    static ProductService productService;
+    Product product = null;
+    Faker faker = new Faker();
+    int id;
+
+    @BeforeAll
+    static void beforeAll() {
+        productService = RetrofitUtils.getRetrofit()
+                .create(ProductService.class);
+    }
+
+    @BeforeEach
+    void setUp() {
+        product = new Product()
+                .withTitle(faker.food().ingredient())
+                .withCategoryTitle("Food")
+                .withPrice((int) (Math.random() * 10000));
+    }
+
+    @Test
+    void deleteProductInFoodCategoryTest() throws IOException {
+        Response<Product> response = productService.createProduct(product)
+                .execute();
+        System.out.println(response.body().getId());
+        id =  response.body().getId();
+        Response<ResponseBody> response2 = productService.deleteProduct(id).execute();
+        assertThat(response2.isSuccessful(), CoreMatchers.is(true));
+    }
+
+
+}
---
+++ src/test/java/lesson5/PutProductTest.java
@@ -0,0 +1,62 @@
+package lesson5;
+
+
+import com.github.javafaker.Faker;
+import lesson5.api.ProductService;
+import lesson5.dto.Product;
+import lesson5.utils.RetrofitUtils;
+import lombok.SneakyThrows;
+import okhttp3.ResponseBody;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import retrofit2.Response;
+import retrofit2.http.Body;
+
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PutProductTest {
+    static ProductService productService;
+    Product product = null;
+    Faker faker = new Faker();
+    int id;
+
+    @BeforeAll
+    static void beforeAll() {
+        productService = RetrofitUtils.getRetrofit()
+                .create(ProductService.class);
+    }
+
+    @BeforeEach
+    void setUp() {
+        product = new Product()
+                .withTitle(faker.food().ingredient())
+                .withCategoryTitle("Food")
+                .withPrice((int) (Math.random() * 10000));
+    }
+
+    @Test
+    void putProductInFoodCategoryTest() throws IOException {
+        Response<Product> response = productService.createProduct(product)
+                .execute();
+        System.out.println(response.body().getId());
+        id =  response.body().getId();
+        Response<Product> response2 = productService.modifyProduct(product.withCategoryTitle("Food"), product.withId(id)).execute();
+        assertThat(response2.isSuccessful(), CoreMatchers.is(true));
+    }
+
+    @SneakyThrows
+    @AfterEach
+    void tearDown() {
+        Response<ResponseBody> response = productService.deleteProduct(id).execute();
+        assertThat(response.isSuccessful(), CoreMatchers.is(true));
+    }
+
+
+}
